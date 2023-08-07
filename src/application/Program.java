55835fa30ce9baf14fae06f9c203e9b51f1d59a2
paulojunior7 <122480101+paulojunior7@users.noreply.github.com>
2023-08-07 19:18:32 -0300

    Project created

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,7 +5,7 @@ public class Program {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Hello Wolrd");
+		System.out.println("Hello Wolrd!");
 
 	}
 
