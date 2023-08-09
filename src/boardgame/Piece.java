4cabfdef510f32657e12c13bb3768ec9345d3682
paulojunior7 <122480101+paulojunior7@users.noreply.github.com>
2023-08-09 17:17:22 -0300

    Starting to implement Board and Piece

---
+++ src/boardgame/Piece.java
@@ -0,0 +1,27 @@
+package boardgame;
+
+public class Piece {
+	
+	protected Position position;
+	private Board board;
+	
+	
+	public Piece(Board board) {
+		this.board = board;
+		position = null;
+	}
+
+
+	public Board getBoard() {
+		return board;
+	}
+
+
+	public void setBoard(Board board) {
+		this.board = board;
+	}
+	
+	
+	
+
+}
